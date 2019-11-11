ad0c61da66e8f89935d669e254d69a6b392704e6
txiang61 <j.tt.xiang@gmail.com>
2019-11-11 16:29:02 -0500

    Merge opprop master to this

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -7,31 +7,26 @@ set -e
 
 export SHELLOPTS
 
-export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
+if [ "$(uname)" == "Darwin" ] ; then
+  export JAVA_HOME=${JAVA_HOME:-$(/usr/libexec/java_home)}
+else
+  export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(readlink -f $(which javac))))}
+fi
 
-export JSR308=`readlink -f ${JSR308:-..}`
-export AFU=`readlink -f ${AFU:-../annotation-tools/annotation-file-utilities}`
-export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
+export JSR308="${JSR308:-$(cd .. && pwd -P)}"
+export AFU="${AFU:-$(pwd -P)/../annotation-tools/annotation-file-utilities}"
+export CHECKERFRAMEWORK="${CHECKERFRAMEWORK:-$(pwd -P)/../checker-framework}"
 
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
 git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
   || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
-eval `/tmp/plume-scripts/ci-info opprop`
 
 ## Build Checker Framework
-if [ -d $CHECKERFRAMEWORK ] ; then
-    # Fails if not currently on a branch
-    git -C $CHECKERFRAMEWORK pull || true
-else
-    REPO=`/tmp/plume-scripts/git-find-fork ${CI_ORGANIZATION} typetools checker-framework`
-    BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${CI_BRANCH}`
-    echo "About to execute: (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO)"
-    (cd $CHECKERFRAMEWORK/.. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO})
-fi
+/tmp/plume-scripts/git-clone-related opprop checker-framework ${CHECKERFRAMEWORK}
 
-# This also builds annotation-tools and jsr308-langtools
-(cd $CHECKERFRAMEWORK && ./.travis-build-without-test.sh downloadjdk jdk8)
+# This also builds annotation-tools
+(cd $CHECKERFRAMEWORK && checker/bin-devel/build.sh downloadjdk jdk8)
 
 # Finally build checker-framework-inference
 ./gradlew dist
