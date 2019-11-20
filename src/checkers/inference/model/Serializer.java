5908ead2b6ce5942a9e48ee08790d0a5f29b5fb3
Jenny Xiang <j.tt.xiang@gmail.com>
2019-11-19 19:57:42 -0500

    Update Serializer.java

--- src/checkers/inference/model/Serializer.java
+++ src/checkers/inference/model/Serializer.java
@@ -26,8 +26,6 @@ public interface Serializer<S, T> {
 
     T serialize(InequalityConstraint constraint);
 
-    S serialize(Slot slot);
-
     S serialize(VariableSlot slot);
 
     S serialize(ConstantSlot slot);
